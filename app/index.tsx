bb05037d0864aaaf6954ffd60ba1d973da4fdd0f
Anshuman Singh Rathore <anshumanrathore328@gmail.com>
2024-08-31 16:36:00 +0530

    setup the project

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { Button, View } from 'react-native';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import { SafeAreaView } from 'react-native-safe-area-context'
+export default function App() {
+  const width = useSharedValue(100);
+
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: 'violet',
+        }}
+      />
+      <Button onPress={handlePress} title="Click me" />
+    </SafeAreaView>
+  );
+}
---
+++ constants/index.ts
@@ -0,0 +1,69 @@
+//images
+import successTransaction from '@/assets/images/successTransaction.png'
+import alfaBank from '@/assets/images/alfa-bank.png'
+import alipay from "@/assets/images/alipay.png"
+import googlePay from "@/assets/images/goggle-pay.png"
+import jcb from "@/assets/images/jcb.png"
+import mastercard from "@/assets/images/mastercard.png"
+import mastero from "@/assets/images/mastero.png"
+import onboarding1 from "@/assets/images/onbording1.png"
+import onboarding2 from "@/assets/images/onbording2.png"
+import profile from "@/assets/images/person.png"
+//icons
+import account from "@/assets/icons/account.png"
+import cart from "@/assets/icons/cart.png"
+import cashier from "@/assets/icons/cashier.png"
+import deleteIcon from "@/assets/icons/delete.png"
+import emplyee from "@/assets/icons/employee.png"
+import history from "@/assets/icons/history.png"
+import layout from "@/assets/icons/layout.png"
+import mail from "@/assets/icons/mail.png"
+import message from "@/assets/icons/message.png"
+import owner from "@/assets/icons/owner.png"
+import phone from "@/assets/icons/phone.png"
+import plus from "@/assets/icons/phone.png"
+import refresh from "@/assets/icons/refresh.png"
+import report from "@/assets/icons/report.png"
+import search from "@/assets/icons/search.png"
+import store from "@/assets/icons/store.png"
+import support from "@/assets/icons/support.png"
+import temp from "@/assets/icons/temp.png"
+import wallet from "@/assets/icons/wallet.png"
+import whatsapp from "@/assets/icons/whatsapp.png"
+import logo from "@/assets/icons/logo.png"
+export const images={
+    successTransaction,
+    alfaBank,
+    alipay,
+    googlePay,
+    jcb,
+    mastercard,
+    mastero,
+    onboarding1,
+    onboarding2,
+    profile,
+    logo,
+    mail,
+    message,
+    owner,
+    phone,
+    plus,
+    refresh,
+    report,
+    search,
+    store,
+    support,
+    temp,
+    wallet,
+    whatsapp
+}
+
+export const icons={
+    account,
+    cart,
+    cashier,
+    deleteIcon,
+    emplyee,
+    history,
+    layout,
+}
